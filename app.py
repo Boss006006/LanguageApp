606914b4dfb28a90dd242414cbc68cdd5b5e8945
Ruben Bos <hrbos1992@gmail.com>
2025-02-01 22:21:15 +0100

    added verbs

--- app.py
+++ app.py
@@ -7,34 +7,53 @@ import time
 import os
 import mysql.connector
 from mysql.connector import Error
+import openpyxl
 
 #endregion
 
 #endregion
  
-#region ---- Page Configuration ----
+#region ---- Home Page and Configuration ----
 st.set_page_config(
         page_title = 'LanguageApp'
     ,   layout = 'wide'
 )
 
+col1, col2 = st.columns([3,1])
+
+col1.title('Mergengues and Schuimgebakjes')
+
+col2.image("image.png"
+    ,   use_container_width=True)
+
+st.write("---")
+
+with st.sidebar:
+    if st.button("Refresh"):
+        st.cache_data.clear()
+        st.rerun()
+
+with st.sidebar:
+    general_menu = option_menu(
+            menu_title='Menu'
+        ,   options = ['Words', 'Input Words', 'Grammar', 'ES Verbs', 'NL Verbs']
+        ,   orientation = 'Vertical'   
+
+    )
+
 #endregion
 
-#region ---- Create database connection
+#region ---- Authentication ----
+
+#endregion
+
+#region ---- Create database connection ----
 
 DB_LOGIN = os.getenv("DB_LOGIN")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-# Debugging line (remove after checking)
-# st.write("Secrets available:", st.secrets)
-
-# Use `.get()` to prevent errors if secrets are missing
 #DB_LOGIN = st.secrets["DB_LOGIN"]
 #DB_PASSWORD = st.secrets["DB_PASSWORD"]
-
-#st.write('TEMP')
-#st.write(DB_LOGIN)
-#st.write(DB_PASSWORD)
 
 db_config = {
     'host': 'sql7.freesqldatabase.com',
@@ -53,30 +72,6 @@ cursor.execute("SHOW TABLES;")
 tables = cursor.fetchall()
 print("Tables:", tables)
 
-#region ---- Authentication ----
-
-#endregion
-
-#region ---- Homepage ----
-st.title('Mergengues and Schuimgebakjes')
-st.write("---")
-
-if st.button("Refresh"):
-    st.cache_data.clear()
-    st.rerun()
-
-with st.sidebar:
-    general_menu = option_menu(
-            menu_title='Menu'
-        ,   options = ['Words', 'Input Words', 'Grammar']
-        ,   orientation = 'Vertical'   
-
-    )
-
-#endregion
-
-### DEV
-
 #TODO: Fix cachin error
 #@st.cache_resource
 def get_connection():
@@ -90,21 +85,22 @@ def get_connection():
         return None
 
 @st.cache_data
-def load_data():
+def load_data(table_name):
     """Load _L_L_WORDS table into a Pandas DataFrame."""
     conn = get_connection()
     if conn:
-        query = "SELECT * FROM _L_L_WORDS;"
+        query = f"SELECT * FROM {table_name}"
         df = pd.read_sql(query, conn)
         conn.close()
         return df
     else:
         return pd.DataFrame() 
+
+#endregion
     
 #region ---- Practice words ----
 
-#TODO: Add parameter to use function for new tables
-df_words = load_data()
+df_words = load_data(table_name='_L_L_WORDS')
 
 if general_menu == 'Words':
     st.title('Practice words')
@@ -128,6 +124,187 @@ if general_menu == 'Words':
         with col3:
             st.markdown(f"<h5 style='color: #333;'>{row['Spanish']}</h5>", unsafe_allow_html=True)
 
+#endregion
 
-### DEV
+#region ---- Spanish Verbs ----
 
+# Create series with indices to slice the dataset on
+df_verbsES = pd.read_excel("Verbs.xlsx", sheet_name= 'Spanish', header=None)
+
+end_value = len(df_verbsES)
+series = {}
+current_series = 1
+series[f'serie{current_series}'] = []
+counter = 0
+
+for number in range(end_value + 1):
+    if counter % 7 == 6:  
+        counter += 1  
+        continue  
+    if len(series[f'serie{current_series}']) == 6: 
+        current_series += 1
+        series[f'serie{current_series}'] = []
+    series[f'serie{current_series}'].append(number)
+    counter += 1
+
+# Create a separate dataframe for each verb
+df_length = len(df_verbsES)
+
+verbs = []
+
+# Loop through each series in the dictionary
+for key, indices in series.items():
+    # Ensure all indices are within the bounds of the DataFrame
+    valid_indices = [index for index in indices if index < df_length]
+    # Create a new dataframe for each series using only valid indices
+    if valid_indices:  # Only if there are valid indices
+        # Temporarily create the dataframe to extract the name and set the column names
+        temp_df = df_verbsES.iloc[valid_indices]
+        # Get the name from the first row, first column
+        df_name = temp_df.iloc[0, 0]
+        verbs.append(df_name)
+        temp_df = temp_df.reset_index(drop=True)
+
+        # Use the first row as the header and the rest as the data
+        temp_df.columns = temp_df.iloc[0]  # Set the first row as the header
+        temp_df = temp_df[1:]  # Drop the first row to remove it as data
+
+        globals()[df_name] = temp_df.reset_index(drop=True)
+        #globals()[df_name].iloc[0, 0] = ''
+
+if general_menu == "ES Verbs":
+
+    # Define times and language
+    languages = ["Espanol", "English"]
+    times = ["Present", "Preterite", "Imperfect", "Future", "Conditional"]
+    
+    st.header("Verbs to learn")
+
+    # Create selection menus
+    selected_verbs = st.multiselect("Select verb", verbs)
+    selected_language = st.multiselect("Select language", languages, default=languages) 
+    selected_times = st.multiselect("Select time", times, default=times)
+
+    first_column_name = 'Infinitive'  
+
+    selected_columns = [f"{time}_{lang[:2].upper()}" for lang in selected_language for time in selected_times]
+
+    # Display
+    for verb_name in selected_verbs:
+        st.write(f"The name of the verb is {verb_name}")
+
+        # Get all columns from the dataframe
+        all_columns = list(globals()[verb_name].columns)
+
+        # The first column name is the same as the verb_name
+        first_column_name = verb_name  
+        
+        # Build the selected columns based on the selected language and time
+        # We first include the first column name to ensure it's always present
+        time_language_columns = [
+            f"{time}_{lang[:2].upper()}" for lang in selected_language for time in selected_times
+        ]
+
+        # Insert the first column at the beginning if it's not already included
+        if first_column_name not in time_language_columns:
+            time_language_columns.insert(0, first_column_name)
+
+        # Filter the all_columns to only include the ones in selected_columns, preserving the original order
+        selected_columns_ordered = [col for col in all_columns if col in time_language_columns]
+
+        # Display the dataframe with the ordered selected columns
+        st.dataframe(globals()[verb_name][selected_columns_ordered])
+
+# endregion
+
+
+#region ---- NL Verbs ----
+
+# Create series with indices to slice the dataset on
+df_verbsNL = pd.read_excel("Verbs.xlsx", sheet_name= 'Dutch', header=None)
+
+end_value = len(df_verbsNL)
+series = {}
+current_series = 1
+series[f'serie{current_series}'] = []
+counter = 0
+
+for number in range(end_value + 1):
+    if counter % 7 == 6:  
+        counter += 1  
+        continue  
+    if len(series[f'serie{current_series}']) == 6: 
+        current_series += 1
+        series[f'serie{current_series}'] = []
+    series[f'serie{current_series}'].append(number)
+    counter += 1
+
+# Create a separate dataframe for each verb
+df_length = len(df_verbsNL)
+
+verbs = []
+
+# Loop through each series in the dictionary
+for key, indices in series.items():
+    # Ensure all indices are within the bounds of the DataFrame
+    valid_indices = [index for index in indices if index < df_length]
+    # Create a new dataframe for each series using only valid indices
+    if valid_indices:  # Only if there are valid indices
+        # Temporarily create the dataframe to extract the name and set the column names
+        temp_df = df_verbsNL.iloc[valid_indices]
+        # Get the name from the first row, first column
+        df_name = temp_df.iloc[0, 0]
+        verbs.append(df_name)
+        temp_df = temp_df.reset_index(drop=True)
+
+        # Use the first row as the header and the rest as the data
+        temp_df.columns = temp_df.iloc[0]  # Set the first row as the header
+        temp_df = temp_df[1:]  # Drop the first row to remove it as data
+
+        globals()[df_name] = temp_df.reset_index(drop=True)
+        #globals()[df_name].iloc[0, 0] = ''
+
+if general_menu == "NL Verbs":
+
+    # Define times and language
+    languages = ["Dutch", "English"]
+    times = ["Present", "Preterite", "Imperfect", "Future", "Conditional"]
+    
+    st.header("Verbs to learn")
+
+    # Create selection menus
+    selected_verbs = st.multiselect("Select verb", verbs)
+    selected_language = st.multiselect("Select language", languages, default=languages) 
+    selected_times = st.multiselect("Select time", times, default=times)
+
+    first_column_name = 'Infinitive'  
+
+    selected_columns = [f"{time}_{lang[:2].upper()}" for lang in selected_language for time in selected_times]
+
+    # Display
+    for verb_name in selected_verbs:
+        st.write(f"The name of the verb is {verb_name}")
+
+        # Get all columns from the dataframe
+        all_columns = list(globals()[verb_name].columns)
+
+        # The first column name is the same as the verb_name
+        first_column_name = verb_name  
+        
+        # Build the selected columns based on the selected language and time
+        # We first include the first column name to ensure it's always present
+        time_language_columns = [
+            f"{time}_{lang[:2].upper()}" for lang in selected_language for time in selected_times
+        ]
+
+        # Insert the first column at the beginning if it's not already included
+        if first_column_name not in time_language_columns:
+            time_language_columns.insert(0, first_column_name)
+
+        # Filter the all_columns to only include the ones in selected_columns, preserving the original order
+        selected_columns_ordered = [col for col in all_columns if col in time_language_columns]
+
+        # Display the dataframe with the ordered selected columns
+        st.dataframe(globals()[verb_name][selected_columns_ordered])
+
+#endregion
